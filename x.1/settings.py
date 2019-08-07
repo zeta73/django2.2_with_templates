da34a7d2dcf9fcfd5b1309b0dbc6fb04f10bb36a
zeta73 <53174991+zeta73@users.noreply.github.com>
2019-08-07 13:34:03 +0700

    Update settings.py

--- x.1/settings.py
+++ x.1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'tolol.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['tolol/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
